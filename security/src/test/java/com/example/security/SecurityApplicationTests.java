ce7db00eaa973c455eeec025efeb72884d49ecbe
ludl4 <ludl4@lenovo.com>
2021-01-07 15:18:30 +0800

    增加了passworencode的实现2

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityApplicationTests {
+public class SecurityApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }

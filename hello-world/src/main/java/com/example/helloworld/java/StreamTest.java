6783fecadefe40d9c1f9d272db2b95c22b99923e
ludl4 <ludl4@lenovo.com>
2021-02-04 13:52:39 +0800

    daf

--- hello-world/src/main/java/com/example/helloworld/java/StreamTest.java
+++ hello-world/src/main/java/com/example/helloworld/java/StreamTest.java
@@ -3,31 +3,60 @@ package com.example.helloworld.java;
 import com.example.helloworld.entity.Student;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Random;
+import javax.xml.crypto.KeySelector;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamTest {
 
+    private static Stream<Integer> limit;
+
     @Test
     public void test0() {
-        List<Student> list = new ArrayList<>();
-        for(int i = 0; i < 100; i++) {
-            list.add(new Student().setAge(i).setName("小明"));
-        }
-        IntSummaryStatistics summarizing = list.stream().collect(Collectors.summarizingInt(Student::getAge));
-        System.out.println(summarizing);
-        Integer collect = list.stream().collect(Collectors.summingInt(Student::getAge));
-        System.out.println(collect);
+//        List<Student> list = new ArrayList<>();
+//        for(int i = 0; i < 100; i++) {
+//            list.add(new Student().setAge(i).setName("小明"));
+//        }
+//        IntSummaryStatistics summarizing = list.stream().collect(Collectors.summarizingInt(Student::getAge));
+//        System.out.println(summarizing);
+//        Integer collect = list.stream().collect(Collectors.summingInt(Student::getAge));
+//        System.out.println(collect);
+        List<Map<Integer, String>> list = new ArrayList<>(10);
+        list.forEach(System.out::println);
 
     }
 
+    /**
+     * 测试有状态操作和和无状态操作之间的影响
+     */
+    @Test
+    public void test1() {
+        Random random = new Random();
+        List<Integer> collect = Stream.generate(random::nextInt)
+                .limit(5)
+                .peek(e -> System.out.println("peek1=" + e))
+                .filter(e -> {
+                    System.out.println("filter=" + e);
+                    return true;
+                })
+                .sorted((a, b) -> {
+                    System.out.println("sorted=" + a + ", " + b);
+                    return a.compareTo(b);
+                })
+                .peek(e -> System.out.println("peek2=" + e))
+                .peek(e -> System.out.println("peek3=" + e))
+                .collect(Collectors.toList());
+
+
+    }
+
+
     public static void main(String[] args) {
-        Stream.iterate(1, i -> i + 1).limit(1000).peek(i -> System.out.println("aa: " + i)).sorted().forEach(System.out::println);
+        limit = Stream.iterate(1, i -> i + 1).limit(1000);
+        Stream<Integer> sorted = limit.peek(i -> System.out.println("aa: " + i)).sorted();
+        sorted.forEach(System.out::println);
         System.out.println("=========================================================");
         //Stream.iterate(1, i -> i + 1).limit(1000).forEach(System.out::println);
         System.out.println("=========================================================");
